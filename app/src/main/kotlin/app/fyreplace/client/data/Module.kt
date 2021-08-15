41961a1ec23df9a37c374fcbf02076180e7f1975
Laurent Tréguier <laurent@treguier.org>
2021-08-15 19:35:15 +0200

    Reuse already resolved context

--- app/src/main/kotlin/app/fyreplace/client/data/Module.kt
+++ app/src/main/kotlin/app/fyreplace/client/data/Module.kt
@@ -19,7 +19,7 @@ val dataModule = module {
                 EncryptedSharedPreferences.create(
                     packageName,
                     MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC),
-                    get(),
+                    this,
                     EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
                     EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
                 ).also { plainTextPrefs.moveTo(it) }
