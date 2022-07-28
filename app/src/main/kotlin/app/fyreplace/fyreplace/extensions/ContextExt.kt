e853d9e78df4e6139307a1cd13ea70c599414828
Laurent Tréguier <laurent.treguier@fyreplace.app>
2022-07-28 17:05:46 +0200

    Use plain text preferences until Tink is fixed

--- app/src/main/kotlin/app/fyreplace/fyreplace/extensions/ContextExt.kt
+++ app/src/main/kotlin/app/fyreplace/fyreplace/extensions/ContextExt.kt
@@ -5,35 +5,15 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.net.Uri
-import android.os.Build
 import androidx.annotation.AttrRes
 import androidx.annotation.ColorRes
 import androidx.core.content.res.use
-import androidx.security.crypto.EncryptedSharedPreferences
-import androidx.security.crypto.MasterKeys
 import app.fyreplace.fyreplace.R
 import com.google.android.material.color.DynamicColors
 import com.google.protobuf.ByteString
 
 val Context.mainPreferences: SharedPreferences
-    get() {
-        val plainTextPrefs = getSharedPreferences(
-            getString(R.string.app_name),
-            Context.MODE_PRIVATE
-        )
-
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            EncryptedSharedPreferences.create(
-                packageName,
-                MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC),
-                this,
-                EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
-                EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
-            ).also { plainTextPrefs.moveTo(it) }
-        } else {
-            plainTextPrefs
-        }
-    }
+    get() = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE)
 
 fun Context.makeShareIntent(type: String, id: ByteString, position: Int? = null): Intent {
     val host = getString(R.string.link_host)
