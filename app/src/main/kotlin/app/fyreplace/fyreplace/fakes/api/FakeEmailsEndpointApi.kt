08123a670a8fdc92c176207f8bdd6719d26c1740
Laurent Tréguier <laurent.treguier@fyreplace.app>
2025-05-01 14:13:07 +0200

    Update API definition

--- app/src/main/kotlin/app/fyreplace/fyreplace/fakes/api/FakeEmailsEndpointApi.kt
+++ app/src/main/kotlin/app/fyreplace/fyreplace/fakes/api/FakeEmailsEndpointApi.kt
@@ -2,17 +2,14 @@ package app.fyreplace.fyreplace.fakes.api
 
 import app.fyreplace.api.EmailsEndpointApi
 import app.fyreplace.api.data.Email
-import app.fyreplace.api.data.EmailActivation
 import app.fyreplace.api.data.EmailCreation
+import app.fyreplace.api.data.EmailVerification
 import app.fyreplace.fyreplace.fakes.make
 import app.fyreplace.fyreplace.fakes.ok
 import retrofit2.Response
 import java.util.UUID
 
 class FakeEmailsEndpointApi : EmailsEndpointApi {
-    override suspend fun activateEmail(emailActivation: EmailActivation): Response<Unit> =
-        throw NotImplementedError()
-
     override suspend fun countEmails() = ok(listEmails(null).body()!!.size.toLong())
 
     override suspend fun createEmail(
@@ -30,5 +27,8 @@ class FakeEmailsEndpointApi : EmailsEndpointApi {
     }
 
     override suspend fun setMainEmail(id: UUID): Response<Unit> =
+        throw NotImplementedError()
+
+    override suspend fun verifyEmail(emailVerification: EmailVerification): Response<Unit> =
         throw NotImplementedError()
 }
