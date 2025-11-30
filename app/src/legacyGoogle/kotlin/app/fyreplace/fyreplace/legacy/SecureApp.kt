3da0368f7e5631509adfe97979aeadf7a26a8180
Laurent Tréguier <laurent.treguier@fyreplace.app>
2025-11-30 12:45:19 +0100

    Avoid crash on automated tests without Google services

--- app/src/legacyGoogle/kotlin/app/fyreplace/fyreplace/legacy/SecureApp.kt
+++ app/src/legacyGoogle/kotlin/app/fyreplace/fyreplace/legacy/SecureApp.kt
@@ -1,11 +1,16 @@
 package app.fyreplace.fyreplace.legacy
 
+import android.content.Intent
 import app.fyreplace.fyreplace.BaseApp
 import com.google.android.gms.security.ProviderInstaller
 
-abstract class SecureApp : BaseApp() {
+abstract class SecureApp : BaseApp(), ProviderInstaller.ProviderInstallListener {
     override fun onCreate() {
         super.onCreate()
-        ProviderInstaller.installIfNeeded(this)
+        ProviderInstaller.installIfNeededAsync(this, this)
     }
+
+    override fun onProviderInstalled() = Unit
+
+    override fun onProviderInstallFailed(errorCode: Int, recoveryIntent: Intent?) = Unit
 }
